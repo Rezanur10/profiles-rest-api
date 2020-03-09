9934e4d1de6f7a9d082ad907b6c5bf441fee6b6a
Rezanur10 <rezanurraihan10@gmail.com>
2020-03-09 23:23:30 +0600

    Added custom user profile model, manager and migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,39 +3,36 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-class UserProfileManager(BaseUserManager):
-    """Managers for user profiles"""
 
-    def create_user(self,email,name,password=None):
-        """create user profile"""
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email,name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,name,password):
-        """Create and save Superuser with given details"""
-        user=self.create_user(email,name,password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-        user.is_superuser=True
-        user.is_staff=True
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
 
 
-
-
-
-
-class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """Database Models for users in the system"""
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -44,16 +41,16 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FILED = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """Retrive short_name of user"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """Return String Representation of user"""
+        """Return string representation of user"""
         return self.email
